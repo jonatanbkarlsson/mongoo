d60fe217b5c6d6ae712b44c45fb3e67eca98d87b
Jonatan Bergström Karlsson <jonatan.karlsson@inspireit.se>
2020-07-14 16:52:54 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sv-SE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mangoo Webbyrå</title>
+
+    <link type="text/css" rel="stylesheet" href="/public/styles/style.css" />
+</head>
+<body>
+    <img src="/public/images/logo.png" alt="Mangoo Webbyrå" />
+</body>
+</html>
